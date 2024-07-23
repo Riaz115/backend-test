a4296b4348d1bcd3d79792f34821617687d65274
Riaz115 <165409821+Riaz115@users.noreply.github.com>
2024-07-23 05:46:43 -0700

    Update MongooseConn.js

--- DB/MongooseConn.js
+++ DB/MongooseConn.js
@@ -4,9 +4,9 @@ const mongoose = require("mongoose");
 
 //variables
 // const URI = "mongodb://127.0.0.1:27017/RiazPortfolioWebsite";
-// const URI = process.env.MONGO_URL;
-const URI =
-  "mongodb://riaz90603:wXTWL3mpNAxTsd2m@ac-6couvvj-shard-00-00.hmtzusf.mongodb.net:27017,ac-6couvvj-shard-00-01.hmtzusf.mongodb.net:27017,ac-6couvvj-shard-00-02.hmtzusf.mongodb.net:27017/RiazPortfolioWebsite?ssl=true&replicaSet=atlas-q12juq-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
+const URI = process.env.MONGO_URL;
+// const URI =
+//   "mongodb://riaz90603:wXTWL3mpNAxTsd2m@ac-6couvvj-shard-00-00.hmtzusf.mongodb.net:27017,ac-6couvvj-shard-00-01.hmtzusf.mongodb.net:27017,ac-6couvvj-shard-00-02.hmtzusf.mongodb.net:27017/RiazPortfolioWebsite?ssl=true&replicaSet=atlas-q12juq-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
 //connecting
 const DbConn = async () => {
   try {
