49b0c05102122368bd227f8cc7a0cac55ec6c109
Riaz115 <165409821+Riaz115@users.noreply.github.com>
2024-07-23 05:40:28 -0700

    first commit

--- Models/RegisterSchema.js
+++ Models/RegisterSchema.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const validator = require("validator");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const forRegisterFormSchema = new mongoose.Schema({
